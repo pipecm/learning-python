b035b456aa2191ce0e2acef925964c219d1ed6da
Felipe Cardenas <pipecm@gmail.com>
2020-06-05 16:52:25 -0400

    Made tic-tac-toe simpler

--- tic-tac-toe/tic_tac_toe.py
+++ tic-tac-toe/tic_tac_toe.py
@@ -1,42 +1,30 @@
-game_board = {
-    'p1': '1', 'p2': '2', 'p3': '3',
-    'p4': '4', 'p5': '5', 'p6': '6',
-    'p7': '7', 'p8': '8', 'p9': '9'
-}
-
-winner_combinations = [
-    {'p1', 'p2', 'p3'},
-    {'p4', 'p5', 'p6'},
-    {'p7', 'p8', 'p9'},
-    {'p1', 'p4', 'p7'},
-    {'p2', 'p5', 'p8'},
-    {'p3', 'p6', 'p9'},
-    {'p1', 'p5', 'p9'},
-    {'p3', 'p5', 'p7'}
-]
-
 cross = 'X'
 nought = 'O'
+game_board = ['#'] + [str(x) for x in range(1, 10)]
+winner_combinations = [
+    {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7},
+    {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}
+]
 
 
 def clear(board):
-    for position in board.keys():
+    for position in range(1, 10):
         board[position] = ' '
 
 
 def display(board):
     print("\n")
-    print(" {} | {} | {} ".format(board['p1'], board['p2'], board['p3']))
+    print(" {} | {} | {} ".format(board[1], board[2], board[3]))
     print("-----------")
-    print(" {} | {} | {} ".format(board['p4'], board['p5'], board['p6']))
+    print(" {} | {} | {} ".format(board[4], board[5], board[6]))
     print("-----------")
-    print(" {} | {} | {} ".format(board['p7'], board['p8'], board['p9']))
+    print(" {} | {} | {} ".format(board[7], board[8], board[9]))
     print("\n")
 
 
 def create_players():
-    player_one = {'name': None, 'symbol': None, 'plays': set()}
-    player_two = {'name': None, 'symbol': None, 'plays': set()}
+    player_one = {'name': None, 'mark': None, 'plays': set()}
+    player_two = {'name': None, 'mark': None, 'plays': set()}
 
     while not player_one['name']:
         player_one['name'] = input("Player 1: Enter your name: ")
@@ -45,36 +33,22 @@ def create_players():
 
     print("Hello, {}".format(player_one['name']))
 
-    while player_one['symbol'] not in (cross, nought):
-        player_one['symbol'] = input("Choose your symbol (X or O): ").upper()
-        if player_one['symbol'] not in (cross, nought):
-            print("Invalid symbol!")
+    while player_one['mark'] not in (cross, nought):
+        player_one['mark'] = input("Choose your mark (X or O): ").upper()
+        if player_one['mark'] not in (cross, nought):
+            print("Invalid mark!")
 
-    print("Good job, {}. You chose {}".format(player_one['name'], player_one['symbol']))
+    print("Good job, {}. You chose {}".format(player_one['name'], player_one['mark']))
 
     while not player_two['name']:
         player_two['name'] = input("Player 2: Enter your name: ")
         if not player_two['name']:
             print("Please enter a non-null name!")
 
-    player_two['symbol'] = cross if player_one['symbol'] == nought else nought
-    print("Hello, {}. You will play with {}".format(player_two['name'], player_two['symbol']))
+    player_two['mark'] = cross if player_one['mark'] == nought else nought
+    print("Hello, {}. You will play with {}".format(player_two['name'], player_two['mark']))
 
     return player_one, player_two
-
-
-def has_won(player):
-    for combination in winner_combinations:
-        if combination.issubset(player['plays']):
-            return True
-    return False
-
-
-def have_tied(board):
-    for position in board.keys():
-        if board[position] not in (cross, nought):
-            return False
-    return True
 
 
 def play(player, board):
@@ -86,15 +60,27 @@ def play(player, board):
         position = int(input("Select the position for your next move: "))
         if position not in range(1, 10):
             print("Invalid position!")
-        elif board['p{}'.format(position)] in (cross, nought):
+        elif board[position] in (cross, nought):
             print("The position {} is already busy. Select other position!".format(position))
             position = 0
         else:
-            board['p{}'.format(position)] = player['symbol']
-            player['plays'].add('p{}'.format(position))
+            board[position] = player['mark']
+            player['plays'].add(position)
             selection_ok = True
 
-# main program
+
+def has_won(player):
+    for combination in winner_combinations:
+        if combination.issubset(player['plays']):
+            return True
+    return False
+
+
+def have_tied(board):
+    for position in range(1, 10):
+        if board[position] not in (cross, nought):
+            return False
+    return True
 
 
 running = True
